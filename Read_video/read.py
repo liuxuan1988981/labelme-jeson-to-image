3bd5e9a5f6d76f7c93fdb4d726317ff4d6e83a22
Liu Xuan <510636115@qq.com>
2022-09-18 13:01:54 +0800

    initial cut

--- Read_video/read.py
+++ Read_video/read.py
@@ -8,24 +8,26 @@ def video2images(Video_Dir):
     if not cap.isOpened():
         print("Cannot open camera")
         exit()
-    if cv2.waitKey(1) == ord('p'):
-        while True:
-            # 逐帧捕获
-            ret, frame = cap.read()
-            # 如果正确读取帧，ret为True
-            if not ret:
-                print("Can't receive frame.")
-                break
-            # 设置每5帧取一次图片，若想逐帧抽取图片，可设置c % 1 == 0
-            if c % 5 == 0:
-                # 图片存放路径，即图片文件夹路径
-                cv2.imwrite('data/' + str(index) + '.jpg', frame) 
-                index += 1
-            c += 1
-            cv2.waitKey(1)
-            # 按键停止
-            if cv2.waitKey(1) == ord('q'):
-                break
+    
+    while True:
+        # 逐帧捕获
+        ret, frame = cap.read()
+        # 如果正确读取帧，ret为True
+        if not ret:
+            print("Can't receive frame.")
+            break
+        # 设置每5帧取一次图片，若想逐帧抽取图片，可设置c % 1 == 0
+        if c % 5 == 0:
+            frame=frame[100:500,100:500,:]
+            # 图片存放路径，即图片文件夹路径
+            # cv2.imwrite('data/' + str(index) + '.jpg', frame) 
+            cv2.imshow("123",frame)
+            index += 1
+        c += 1
+        cv2.waitKey(1)
+        # 按键停止
+        if cv2.waitKey(1) == ord('q'):
+            break
     cap.release()
  
 def playvideo(Video_Dir):
@@ -43,5 +45,5 @@ def playvideo(Video_Dir):
 
 if __name__ == "__main__":
     Video_Dir = "1.mkv"
-    # video2images(Video_Dir)
-    playvideo(Video_Dir)
+    video2images(Video_Dir)
+    # playvideo(Video_Dir)
